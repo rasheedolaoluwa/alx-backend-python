497ca9551cb3741731e819861cd0d37169130302
Rasheed <radeyola@gmail.com>
2024-08-01 19:59:43 +0000

    update add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Basic type annotations for variables.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+    """
+    return a + b
