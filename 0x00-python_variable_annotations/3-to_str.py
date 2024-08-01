6844f4b702f9e3f4585f67c028f1d916c73705f6
Rasheed <radeyola@gmail.com>
2024-08-01 18:42:59 +0000

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Basic type annotations for converting to string. """
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of a float. """
+    return str(n)
