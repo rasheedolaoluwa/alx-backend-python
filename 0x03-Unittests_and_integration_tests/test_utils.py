6ae42ff6d35c3904935dcd9bf6f32a6efd7649ec
Rasheed <radeyola@gmail.com>
2024-08-26 09:52:21 +0000

    test OK

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" Module for testing utils """
+
+from parameterized import parameterized
+import unittest
+from unittest.mock import patch
+from utils import (access_nested_map, get_json, memoize)
+import requests
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Class for testing access_nested_map """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test that access_nested_map returns the correct value """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for missing keys """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class for testing get_json """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test that get_json returns the correct payload """
+        config = {'return_value.json.return_value': test_payload}
+        patcher = patch('requests.get', **config)
+        mock = patcher.start()
+        self.assertEqual(get_json(test_url), test_payload)
+        mock.assert_called_once()
+        patcher.stop()
+
+
+class TestMemoize(unittest.TestCase):
+    """ Class for testing memoize """
+
+    def test_memoize(self):
+        """ Test that memoize caches the result of a_property correctly """
+
+        class TestClass:
+            """ Test class for memoize """
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
